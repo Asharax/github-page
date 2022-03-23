adc501b36aa471b596375e5559baab0bed804601
Furkan Aytan <aytan@editsystems.com>
2022-03-24 00:19:56 +0300

    Added buttons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,21 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import '@fortawesome/fontawesome-free/css/all.css' // Ensure your project is capable of handling css files
 
 // Vuetify
 import { createVuetify } from 'vuetify'
+import { aliases, fa } from 'vuetify/lib/iconsets/fa'
+import { mdi } from 'vuetify/lib/iconsets/mdi'
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        fa,
+        mdi,
+      }
+    },
+  }
 )
